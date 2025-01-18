39e96928b8bd416533ae51c3b6df7fbba66fc397
Matt Bos <matt.bos1997@gmail.com>
2025-01-18 20:36:25 +1100

    Removed put post and delete endpoints
    Seeing as no authentication is yet is place. All editing requests are not added.

--- Models/Person.cs
+++ Models/Person.cs
@@ -7,11 +7,17 @@ public class Person
 {
     
     public long Id { get; set; }
+    
+    [Required]
     [MaxLength(50)]
     public required string FirstName { get; set; }
+    
+    [Required]
     [MaxLength(50)]
     public required string LastName { get; set; }
+    
     public DateOnly DateOfBirth { get; set; }
+    
     public required string Bio { get; set; }
     
     [JsonIgnore]
